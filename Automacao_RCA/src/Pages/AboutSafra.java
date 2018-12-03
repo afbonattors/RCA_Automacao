1926f3e3ab8da44da10dc2b0c7ba98e83c587edf
admin <andre.bonatto@operacao.rcadigital.com.br>
2018-12-03 12:08:32 -0200

    Commit

---
+++ Automacao_RCA/src/Pages/AboutSafra.java
@@ -0,0 +1,16 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import Utils.BaseTest;
+
+public class AboutSafra extends BaseTest {
+	
+	public static WebElement divDescription () {
+		
+		return BaseTest.getDriver().findElement(By.xpath("//*[@id=\"about\"]/div[2]"));
+		
+	}
+
+}
