52d13f87961bc603beea99a667b0692f7dfe1a48
admin <andre.bonatto@operacao.rcadigital.com.br>
2018-12-14 14:13:03 -0200

    Test Safra Origins

--- Automacao_RCA/src/Test/SafraOriginsTest.java
+++ Automacao_RCA/src/Test/SafraOriginsTest.java
@@ -15,16 +15,27 @@ public class SafraOriginsTest extends BaseTest {
 	public void originsTest() throws Exception {
 		
 		System.out.println("Teste Origins\n");
+		
 		String PageTitle = getDriver().getTitle();
+		
 		Assert.assertTrue(PageTitle.equals("Safra National Bank"));
+		
 		ScriptHeader.testHeader();
+		
 		Thread.sleep(1000);
+		
 		ScriptFooter.testFooter();
+		
 		Thread.sleep(1000);
+		
 		SafraOrignsScript.clickAbout();
+		
 		Thread.sleep(1000);
+		
 		SafraOrignsScript.clickOrigins();
+		
 		Thread.sleep(1000);
+		
 		Assert.assertTrue(SafraOriginsPage.divDescription().isDisplayed());		
 		
 	}
